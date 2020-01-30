93ec612bbe05fccc6bc015ec72924ad8bdf5128f
kaitmore <kaitmore@gmail.com>
2020-01-30 17:26:09 -0500

    try cookie

--- auth/index.js
+++ auth/index.js
@@ -1,14 +1,13 @@
 const express = require("express");
-const auth = require("basic-auth");
 const app = express();
+const cookie = require("cookie");
 const port = 9090 || process.env.PORT;
 
 app.use((req, res, next) => {
   console.log(req.headers);
-  console.log(auth(req));
   try {
-    const { name, pass } = auth(req);
-    if (name === "ricklee" && pass === "123456") {
+    const { name, pass } = cookie.parse(req.headers["cookie"]);
+    if (name === "kait" && pass === "123456") {
       res.status(200);
       res.end();
     } else {
